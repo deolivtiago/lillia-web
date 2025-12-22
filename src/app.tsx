e5c07576bdd5e29053b60cae3614d2a928b4da76
Tiago de Oliveira <deolivtiago@outlook.com>
2025-12-22 16:12:16 -0300

    chore: include tailwind

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export function App() {
+  return (
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center bg-cyan-950">
+        <h1 className="m-4 text-3xl font-bold text-white">Hello!</h1>
+        <Button className="bg-primary font-bold">DON'T CLICK ME</Button>
+      </div>
+    </>
+  );
+}
