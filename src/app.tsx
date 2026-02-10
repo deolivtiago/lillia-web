ce783bfc8ad61ede69a01b236fd14dae68e514dd
Tiago de Oliveira <deolivtiago@outlook.com>
2026-02-09 23:08:37 -0300

    chore: include prettier

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"
 
 export function App() {
   return (
     <>
-      <div className="max-w-7xl mx-auto p-4 h-screen shadow-xl space-y-4">
+      <div className="mx-auto h-screen max-w-7xl space-y-4 p-4 shadow-xl">
         <h1 className="font-serif text-3xl font-bold">Hello</h1>
         <h2 className="font-cursive text-xl font-bold">World</h2>
         <p className="font-sans">
@@ -12,11 +12,11 @@ export function App() {
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </p>
-        <p className="font-mono text-muted-foreground">
+        <p className="text-muted-foreground font-mono">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
         <Button>DON'T CLICK</Button>
       </div>
     </>
-  );
+  )
 }
