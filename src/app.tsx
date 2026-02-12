5abe22f41d55a069b4ff2ba48fe75814e57410c5
Tiago de Oliveira <deolivtiago@outlook.com>
2026-02-11 22:15:12 -0300

    feat: add page to sign in

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,10 @@ import { createBrowserRouter, RouterProvider } from "react-router"
 import { AuthProvider } from "@/providers/auth-provider"
 import { ThemeProvider } from "@/providers/theme-provider"
 
+import { Auth } from "@/layouts/auth"
 import { Root } from "@/layouts/root"
 
+import { SignIn } from "@/pages/auth/sign-in"
 import { Home } from "@/pages/home"
 import { NotFound } from "@/pages/not-found"
 
@@ -12,6 +14,15 @@ import { AppRoutes } from "@/config/app-routes"
 import { AppStorage } from "@/config/app-storage"
 
 const router = createBrowserRouter([
+  {
+    path: AppRoutes.Auth.path,
+    Component: Auth,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, Component: NotFound },
+      { path: AppRoutes.SignIn.path, Component: SignIn },
+    ],
+  },
   {
     path: AppRoutes.Home.path,
     Component: Root,
