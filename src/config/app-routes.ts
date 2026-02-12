ae52421e15ce3141845b155883e83d75c0b8315f
Tiago de Oliveira <deolivtiago@outlook.com>
2026-02-11 22:08:39 -0300

    feat: add app routes for authentication

--- src/config/app-routes.ts
+++ src/config/app-routes.ts
@@ -2,10 +2,14 @@ export type AppRoute = { label: string; path: string }
 export type AppRoutes = (typeof AppRoutes)[keyof typeof AppRoutes]
 
 export const AppRoutes = {
-  Home: { label: "Home", path: "/" },
+  Home: { path: "/", label: "Home" },
+  Auth: { path: "/auth", label: "Auth" },
 
   SignIn: { path: "/auth/sign-in", label: "Sign In" },
   SignUp: { path: "/auth/sign-up", label: "Sign Up" },
+
+  TermsOfUse: { path: "/auth/terms-of-use", label: "Terms of Use" },
+  PrivacyPolicy: { path: "/auth/privacy-policy", label: "Privacy Policy" },
 
   items: function (): AppRoute[] {
     return Object.values(this).filter((it) => typeof it !== "function")
